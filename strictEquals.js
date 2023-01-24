96e91b0dec27736c0b7becbb97c907a966e8059a
SemperFenix <dureivan@gmail.com>
2023-01-24 19:25:35 +0100

    Fix sonarCloud warnings

--- strictEquals.js
+++ strictEquals.js
@@ -1,36 +1,15 @@
 export const strictEquals = (a, b) => {
   let result = "";
   if (isNaN(a)) {
-    result = "false";
-  } else {
-    switch (a) {
-      case 0:
-        if (Object.is(b, 0) || Object.is(b, -0)) {
-          result = "true";
-        } else {
-          result = "false";
-        }
-
-        break;
-
-      case -0:
-        if (Object.is(b, 0) || Object.is(b, -0)) {
-          result = "true";
-        } else {
-          result = "false";
-        }
-
-        break;
-
-      default:
-        if (Object.is(a, b)) {
-          result = "true";
-        } else {
-          result = "false";
-        }
-
-        break;
+    result = false;
+  } else if (Object.is(a, 0) || Object.is(a, -0)) {
+    if (Object.is(b, -0) || Object.is(b, 0)) {
+      result = true;
+    } else {
+      result = false;
     }
+  } else {
+    result = Object.is(a, b);
   }
 
   return console.log(result);
