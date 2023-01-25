bb65d232bba84059403864fdd70d1ae0801fbccd
SemperFenix <dureivan@gmail.com>
2023-01-25 10:48:29 +0100

    Refactor strictEquals with ternary operators

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 import { strictEquals } from "./strictEquals.js";
 
 console.log("La primera comparación: 1, 1");
-strictEquals(1, 1);
+console.log(strictEquals(1, 1));
 console.log("La segunda comparación: NaN, NaN");
-strictEquals(NaN, NaN);
+console.log(strictEquals(NaN, NaN));
 console.log("La tercera comparación: 0, -0");
-strictEquals(0, -0);
+console.log(strictEquals(0, -0));
 console.log("La cuarta comparación: -0, 0");
-strictEquals(-0, 0);
+console.log(strictEquals(-0, 0));
 console.log("La quinta comparación: 1, '1'");
-strictEquals(1, "1");
+console.log(strictEquals(1, "1"));
 console.log("La sexta comparación: true, false");
-strictEquals(true, false);
+console.log(strictEquals(true, false));
 console.log("La séptima comparación: false, false");
-strictEquals(false, false);
+console.log(strictEquals(false, false));
 console.log("La octava comparación: water, oil");
-strictEquals("Water", "oil");
+console.log(strictEquals("Water", "oil"));
--- strictEquals.js
+++ strictEquals.js
@@ -1,16 +1,17 @@
 export const strictEquals = (a, b) => {
   let result = "";
-  if (isNaN(a)) {
-    result = false;
-  } else if (Object.is(a, 0) || Object.is(a, -0)) {
-    if (Object.is(b, -0) || Object.is(b, 0)) {
-      result = true;
-    } else {
-      result = false;
-    }
-  } else {
-    result = Object.is(a, b);
-  }
 
-  return console.log(result);
+  result = Object.is(a, NaN)
+    ? (result = false)
+    : (result =
+        Object.is(a, 0) && Object.is(b, -0)
+          ? (result = true)
+          : (result =
+              Object.is(a, -0) && Object.is(b, 0)
+                ? (result = true)
+                : (result = Object.is(a, b)
+                    ? (result = true)
+                    : (result = false))));
+
+  return result;
 };
